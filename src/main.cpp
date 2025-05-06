92f6c8998ce4ccd95ca1aab52736df2adf40d61d
PortalPC <ian.portal@usil.pe>
2025-05-05 19:49:33 -0500

    Actualización_Portal_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
 
-// put function declarations here:
-int myFunction(int, int);
+volatile uint8_t estado = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int main(void){
+DDRD |= ( 1 << PD6)|(1<<PD7);
+PORTD &= ~((1<< PD6) | (1<<PD7));
+
+DDRD &= ~((1<<PD2)) | (1<<PD2);
+PORTD |= (1<<PD2);
+
+EICRA |= (1<<ISC01);
+EIMSK |= (1<<INT0);
+
+sei();
+
+while(1){
+  switch (estado){
+    case 0:
+
+    PORTD &= ~((1<<PD6)|(1<<PD7));
+    break;
+    case 1:
+  }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
